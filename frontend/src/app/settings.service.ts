12930a87b119ad2612161127f0d1c109f7da5a8d
lukalajovic12 <lukec.lajovic@gmail.com>
2024-01-29 18:36:11 +0100

    created docs to run build on github

--- frontend/src/app/settings.service.ts
+++ frontend/src/app/settings.service.ts
@@ -40,45 +40,11 @@ export class SettingsService {
       timeIncrease:this.timeIncrease
     };
       
-      const httpOptions = {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${this._userService.token}`
-        })
-      };   
-      
-      // Subscribe to the POST request to trigger it
-      this.http.post<GameSettings>(environment.BACKEND_URL+"api/settings/", setting, httpOptions).subscribe(
-        (response) => {
-          // Handle the response from the server (e.g., update your local todos)
-          console.log('settings saved:', response);
-        },
-        (error) => {
-          // Handle errors here
-          console.error('Error saving settings:', error);
-        }
-      );
+
     }
 
     public async getData(title:string ):Promise<void> {
-      this.title=title;
-      let url =environment.BACKEND_URL+"api/settings_get/?title="+this.title+"&"+"user_id="+this._userService.user_id;      
-      let settingsList: Observable<GameSettings[]>;
-      let headers = new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${this._userService.token}`
-      });   
-      settingsList=this.http.get<GameSettings[]>(url, { headers });
-      settingsList.subscribe(settings => {
-        if (settings.length === 0) {
-          this.saveSettings();
-        } else {
-          this.lives = settings[0].lives;
-          this.startLevel = settings[0].startLevel;
-          this.mistakes = settings[0].mistakes;
-          this.settings_id = settings[0].id;
-        }
-      });
+  
     }
 
 }
